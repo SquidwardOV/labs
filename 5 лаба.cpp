e1cbe93b7ef5c9c14a80b72787a9e82d9f9ed67b
SquidwardOV <115181787+SquidwardOV@users.noreply.github.com>
2022-11-24 22:31:59 +0300

    Update 5 лаба.cpp

--- 5 лаба.cpp
+++ 5 лаба.cpp
@@ -1,31 +1,66 @@
 #include <iostream>
-using namespace std;
- 
-int main()
-{
-    int k = 0, n;
-    int arr[n], res[n];
-    int count;
-    cout << "Введите количество символов в массиве: ";
-    cin>>n;
-    for(int i = 0; i < n; i++)
-        cin >> arr[i];
-    for(int i = 0; i < n-1; i++)
-    {
-        count = 0;
-        for(int j = 1; j < n; j++)
-        {
-            if(arr[i] == arr[j] && i != j)
-                count++;
-        }
-        if(count == 0)
-        {
-            res[k] = arr[i];
-            k++;
-        }
-    }
-    for(int i = 0; i < k; i++)
-        cout << res[i] << " ";
-        
-  }
 
+
+using namespace std;
+
+void inclusionSort (int *num, int size){
+    for (int i = 1; i < size; ++i){
+        int value = num[i];
+        int index = i;
+        while ((index > 0) && (num[index - 1] > value)){
+            num[index] = num[index - 1];
+            index--;
+        }
+        num[index] = value;
+    }
+}
+
+
+int main(){
+
+    int size, count = 0, k = 0;
+
+    cout << "Введите количество элементов в изначальном массиве: ";
+    cin >> size;
+
+    int* arr = new int[size];
+    int* new_arr = new int[size];
+
+    cout << "Введите элементы массива: ";
+    for (int i = 0; i < size; ++i) {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < size; ++i) {
+
+        for (int j = 0; j < size; ++j) {
+
+            if (arr[i] == arr[j] && i != j) {
+                ++count;
+            }
+        }
+        if (count == 0) {
+            new_arr[k] = arr[i];
+            ++k;
+
+        }
+        count = 0;
+    }
+
+
+int* res_arr = new int[k];
+
+for (int i = 0; i < k; ++i) {
+res_arr[i] = new_arr[i];
+}
+delete[] arr;
+delete[] new_arr;
+
+for (int i = 0; i < k; ++i) {
+cout << res_arr[i] << " ";
+}
+
+delete[] res_arr;
+
+
+}
